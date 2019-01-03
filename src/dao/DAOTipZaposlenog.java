93b6ff2167460c222d62f6c6b77d4d718baedd73
tijanalakic <42244128+tijanalakic@users.noreply.github.com>
2019-01-03 21:48:37 +0100

    DAO tip Zaposlenog

---
+++ src/dao/DAOTipZaposlenog.java
@@ -0,0 +1,100 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package dao;
+
+import dto.DTOTipZaposlenog;
+import java.sql.Connection;
+import connectionpool.ConnectionPool;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+
+/**
+ *
+ * @author Tijana Lakic
+ */
+public class DAOTipZaposlenog {
+
+    public final String SQL_GET_TIP_ZAPOSLENOG = "select * from baby_shop.tip_zaposlenog";
+
+    public ObservableList<DTOTipZaposlenog> getTipZaposlenog() {
+
+        Connection con = null;
+        PreparedStatement ps = null;
+        ResultSet rs = null;
+        ArrayList<DTOTipZaposlenog> tipzaposlenog = new ArrayList<>();
+
+        try {
+            con = ConnectionPool.getInstance().checkOut();
+            ps = con.prepareStatement(SQL_GET_TIP_ZAPOSLENOG);
+            rs = ps.executeQuery();
+            while (rs.next()) {
+                int idTipaZaposlenog = rs.getInt(1);
+                String nazivTipaZaposlenog = rs.getString(2);
+                tipzaposlenog.add(new DTOTipZaposlenog(idTipaZaposlenog, nazivTipaZaposlenog));
+            }
+        } catch (SQLException ex) {
+            Logger.getLogger(DAOTipZaposlenog.class.getName()).log(Level.SEVERE, null, ex);
+        } finally {
+            if (con != null) {
+                ConnectionPool.getInstance().checkIn(con);
+            }
+            if (ps != null) {
+                try {
+                    ps.close();
+                } catch (SQLException ex) {
+                    Logger.getLogger(DAOTipZaposlenog.class.getName()).log(Level.SEVERE, null, ex);
+                }
+            }
+            if (rs != null) {
+                try {
+                    rs.close();
+                } catch (SQLException ex) {
+                    Logger.getLogger(DAOTipZaposlenog.class.getName()).log(Level.SEVERE, null, ex);
+                }
+            }
+        }
+        return FXCollections.observableArrayList(tipzaposlenog);
+
+    }
+    
+    public boolean dodajTipZaposlenog(int idTipaZaposlenog,String nazivTipaZaposlenog){
+    
+   
+            Connection con=null;
+            PreparedStatement ps=null;
+            try {     
+            con=ConnectionPool.getInstance().checkOut();
+            ps=con.prepareStatement("insert into baby_shop.tip_zaposlenog "
+                    + "(IdTipa,NazivTipa) VALUES (default,?)");
+            ps.setString(1, nazivTipaZaposlenog);
+        } catch (SQLException ex) {
+            Logger.getLogger(DAOTipZaposlenog.class.getName()).log(Level.SEVERE, null, ex);
+        }finally {
+            if (con != null) {
+                try {
+                    con.close();
+                } catch (SQLException ex) {
+                    Logger.getLogger(DAOTipZaposlenog.class.getName()).log(Level.SEVERE, null, ex);
+                }
+            }
+            if (ps != null) {
+                try {
+                    ps.close();
+                } catch (SQLException ex) {
+                    Logger.getLogger(DAOTipZaposlenog.class.getName()).log(Level.SEVERE, null, ex);
+                }
+            }
+        }
+        return true;
+    
+    }
+}
