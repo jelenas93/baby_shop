6afdf270d381691ad60b2f1a712bac713c925357
Jelena <Jelena@Jelena.mshome.net>
2019-01-04 13:04:07 +0100

    tijana evo da vidis okvirno za gui da mozes nastaviti

---
+++ src/gui/PregledProizvodaController.java
@@ -0,0 +1,14 @@
+package gui;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.Initializable;
+
+public class PregledProizvodaController implements Initializable {
+
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+     
+    }    
+    
+}
