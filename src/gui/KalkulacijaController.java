bc72cde3b9cd31e6670d08cc5b730b6fe4825e47
Marija <Marija@DESKTOP-OJBJBFP>
2019-01-24 16:40:28 +0100

    kalkulacija

--- src/gui/KalkulacijaController.java
+++ src/gui/KalkulacijaController.java
@@ -12,6 +12,7 @@ import dto.DTONarudzbenica;
 import dto.DTOProizvod;
 import dto.DTOStavkaKalkulacije;
 import dto.DTOStavkaNarudzbe;
+import java.io.IOException;
 import java.net.URL;
 import java.util.ArrayList;
 import java.util.Date;
@@ -20,16 +21,22 @@ import java.util.List;
 import java.util.ResourceBundle;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
 import javafx.fxml.Initializable;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
+import javafx.scene.Node;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.Label;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
 import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.stage.Stage;
 import pdf.PDF;
 import tabele.TabelaKalkulacija;
 
@@ -314,7 +321,12 @@ public class KalkulacijaController implements Initializable {
             }
         }
     }
-    public void nazadStisak(){
-        
+    public void nazadStisak(ActionEvent event) throws IOException{
+        Parent korisnikView = FXMLLoader.load(getClass().getResource("/gui/PocetnaForma.fxml"));
+        Stage window = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        Scene korisnikScena = new Scene(korisnikView);
+        window.setScene(korisnikScena);
+        window.centerOnScreen();
+        window.show();
     }
 }
