bd3d8a5f32d7c7f4be52b2b5f878790c0a99fdd6
Jelena <Jelena@Jelena.mshome.net>
2019-02-28 12:28:17 +0100

    pola gui-ja jelena

--- src/gui/KasirFormaController.java
+++ src/gui/KasirFormaController.java
@@ -1,16 +1,34 @@
-
 package gui;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
 
 public class KasirFormaController implements Initializable {
 
     public static int idKasira;
+
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
-    }    
-    
+    }
+
+    @FXML
+    private void odjava(ActionEvent event) throws IOException {
+        Parent korisnikView = FXMLLoader.load(getClass().getResource("/gui/prijavaNaSistem.fxml"));
+        Scene korisnikScena = new Scene(korisnikView);
+        Stage window = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        window.setScene(korisnikScena);
+        window.centerOnScreen();
+        window.show();
+    }
+
 }
