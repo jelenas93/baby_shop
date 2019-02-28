2952d8bf2ae4fbf00a736b31c4c3917d3419449a
Jelena <Jelena@Jelena.mshome.net>
2019-02-28 15:53:16 +0100

    novi css

--- src/babyshop/Babyshop.java
+++ src/babyshop/Babyshop.java
@@ -1,25 +1,24 @@
 package babyshop;
 
 import java.io.IOException;
+import java.nio.file.Paths;
 import java.util.Date;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 import pdf.PDF;
 
 public class Babyshop extends Application {
-    
+
     @Override
     public void start(Stage primaryStage) throws IOException {
         Parent root = FXMLLoader.load(getClass().getResource("/gui/prijavaNaSistem.fxml"));
-       // PDF.kreirajIzvjestajPoDobavljacuZaMjesec(new Date(119,01,21), new Date(119, 03, 24), 1);
-
-    //  Parent root = FXMLLoader.load(getClass().getResource("/gui/pregledKorisnickogNaloga.fxml"));
-        
-       //Parent root = FXMLLoader.load(getClass().getResource("/gui/unosZaposlenog.fxml"));
-        //primaryStage.getIcons().add(new Image("file:" + String.valueOf(Paths.get(System.getProperty("user.dir"), "film.png"))));
+        //Parent root = FXMLLoader.load(getClass().getResource("/gui/unosZaposlenog.fxml"));
+        primaryStage.setTitle("Prijava na sistem");
+        primaryStage.getIcons().add(new Image("file:" + String.valueOf(Paths.get(System.getProperty("user.dir"), "logo.jpg"))));
         Scene scene = new Scene(root);
         primaryStage.setScene(scene);
         primaryStage.centerOnScreen();
@@ -29,5 +28,5 @@ public class Babyshop extends Application {
     public static void main(String[] args) {
         launch(args);
     }
-    
+
 }
